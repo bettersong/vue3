09eb17c2a33bc4e5cf432449f3e8671d801122cb
宋瑶 <songyao@qutoutiao.net>
2020-10-13 17:01:37 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
 import { defineAsyncComponent } from "vue";
 export default {
   name: "App",
@@ -19,13 +18,4 @@ export default {
 };
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less"></style>
